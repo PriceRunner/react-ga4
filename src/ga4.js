4ca60a77473703c14b8abb6b4fdcb67deb775cd0
Lu Yu <nluyu@amazon.com>
2022-11-26 16:02:06 -0800

    embed gtag in code
    Signed-off-by: Lu Yu <nluyu@amazon.com>

--- src/ga4.js
+++ src/ga4.js
@@ -87,7 +87,7 @@ export class GA4 {
       // Global Site Tag (gtag.js) - Google Analytics
       const script = document.createElement("script");
       script.async = true;
-      script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+      script.src = `./ga_source.js`;
       if (nonce) {
         script.setAttribute("nonce", nonce);
       }
